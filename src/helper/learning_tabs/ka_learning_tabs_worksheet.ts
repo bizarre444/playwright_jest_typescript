a8fcc099527aeb833be8ee886539951cd5d796c9
bizarre999 <bizarre444bizarre@gmail.com>
2022-03-25 12:28:25 +0300

    fixed worksheet page title

--- src/helper/learning_tabs/ka_learning_tabs_worksheet.ts
+++ src/helper/learning_tabs/ka_learning_tabs_worksheet.ts
@@ -4,7 +4,7 @@ let learning_tabs_worksheet: LearningTabs = {
     url: "https://www.ka-stage.ml/free-preschool-kindergarten-worksheets/",
     name: "worksheets",
     tag_learning: "Printable worksheets",
-    text_learning: "Free Preschool & Kindergarten Worksheets and Printables",
+    text_learning: "Free & Fun Worksheets for Kids",
     title_learning_tag: ".learning-resources__primary-title"
 }
 
